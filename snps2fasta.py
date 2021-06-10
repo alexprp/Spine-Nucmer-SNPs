56c546ab05cc53fdadc21819d8004b874ccf27f2
alexprp <alexprphillips@gmail.com>
2021-06-10 11:10:42 -0500

    temporarily removed main() funciton to more directly observe/manipulate data structures in interpreter

--- snps2fasta.py
+++ snps2fasta.py
@@ -112,7 +112,6 @@ def parse_snps(ref_fasta, ref_snps_obj, file, genomeid):
 					ref_snps_obj.snps[backbone][pos] = refnuc
 				query_snps_obj.snps[backbone][pos] = "-"
 
-	insnps.close()
 
 	return ref_snps_obj, query_snps_obj
 
@@ -215,6 +214,9 @@ def make_snp_matrix(snps_obj_list):
 
 	return snp_matrix
 
+#def remove_singletons(snp_matrix):
+
+
 def make_snp_matrix_multifasta(snps_obj_list):
 	"""
 	Flattens all the dictionaries of snps in each snps_object class and adds them to a single subdict associated with their genome ID as a key in the main dict {genome_id : {contig : sequence}}
@@ -261,167 +263,165 @@ def fill_ref_dict_with_seq(ref_snps_obj, ref_seq_dict):
 
 	
 
-def main():
+#$def main():
 
 
-	parser = argparse.ArgumentParser(
-		description="After running spine on a set of genomes to find the core genome and then running nucmer show-snps to identify differences in core genome sequence between genomes, this script converts all the .snps files output by nucmer into a fasta formatted alignment of just the variable sites in each backbone sequences.\
-		\n\nExample usage : snps2fasta.py -r Spine/backbone.fasta -f Out/alignment.fasta -whole -m Out/snp_matrix.csv -d '\\t' -p '(.*)_core\.snps' Nucmer/*.snps",
-		formatter_class=argparse.RawTextHelpFormatter)
-	parser.add_argument(
-		"-r",  dest="reference", required = True,
-		help="Specify reference backbone fasta file. "
-		)
-	parser.add_argument(
-		"-f", dest="out_fasta", required = False,
-		help="Specify output fasta filename."
-		)
-	parser.add_argument(
-		"-m", dest="out_matrix", required = False, 
-		help="Specify output matrix filename."
-		)
-	parser.add_argument(
-		"-d", dest="matrix_delim", nargs="?", default = ',',
-		help="Specify output matrix delimeter. Default comma delimeted"
-		)
-	parser.add_argument(
-		"-p", dest="filename_regex", nargs="?", default = r'(.+)',
-		help="Specify regex pattern to capture genome ID from filename for use as fasta header and rowname in snp matrix. Default behavior is to use the whole filename."
-		)
-	parser.add_argument(
-		"-whole", dest="whole", action='store_true',  
-		help="Specify Whether you want the output fasta to be the entire core sequence or just the variant sites. "
-		)
-	parser.add_argument(
-		"snps_files", nargs="+",  
-		help="Specify input core.snps files produced by nucmer. "
-		)
-	parser.add_argument(
-		"-multifasta", dest="multifasta", action='store_true',  
-		help="Specify Whether you want the output fasta to be the each aligned core segment in a separate file. With this option you will need to provide an output directory into which one file per core segment will be produced. Each file will contain one fasta sequence per genome corresponding to an aligned core segment. Can be combined with the -whole option to control if you only want variant sites or the whole core genome."
-		)
-	parser.add_argument(
-		"-o", dest="out_dir", required = False,
-		help="If you chose the -multifasta option, this option can be used to define the path to the directory you want the multifastas written."
-		)
-	
+parser = argparse.ArgumentParser(
+	description="After running spine on a set of genomes to find the core genome and then running nucmer show-snps to identify differences in core genome sequence between genomes, this script converts all the .snps files output by nucmer into a fasta formatted alignment of just the variable sites in each backbone sequences.\
+	\n\nExample usage : snps2fasta.py -r Spine/backbone.fasta -f Out/alignment.fasta -whole -m Out/snp_matrix.csv -d '\\t' -p '(.*)_core\.snps' Nucmer/*.snps",
+	formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument(
+	"-r",  dest="reference", required = True,
+	help="Specify reference backbone fasta file. "
+	)
+parser.add_argument(
+	"-f", dest="out_fasta", required = False,
+	help="Specify output fasta filename."
+	)
+parser.add_argument(
+	"-m", dest="out_matrix", required = False, 
+	help="Specify output matrix filename."
+	)
+parser.add_argument(
+	"-d", dest="matrix_delim", nargs="?", default = ',',
+	help="Specify output matrix delimeter. Default comma delimeted"
+	)
+parser.add_argument(
+	"-p", dest="filename_regex", nargs="?", default = r'(.+)',
+	help="Specify regex pattern to capture genome ID from filename for use as fasta header and rowname in snp matrix. Default behavior is to use the whole filename."
+	)
+parser.add_argument(
+	"-whole", dest="whole", action='store_true',  
+	help="Specify Whether you want the output fasta to be the entire core sequence or just the variant sites. "
+	)
+parser.add_argument(
+	"snps_files", nargs="+",  
+	help="Specify input core.snps files produced by nucmer. "
+	)
+parser.add_argument(
+	"-multifasta", dest="multifasta", action='store_true',  
+	help="Specify Whether you want the output fasta to be the each aligned core segment in a separate file. With this option you will need to provide an output directory into which one file per core segment will be produced. Each file will contain one fasta sequence per genome corresponding to an aligned core segment. Can be combined with the -whole option to control if you only want variant sites or the whole core genome."
+	)
+parser.add_argument(
+	"-o", dest="out_dir", required = False,
+	help="If you chose the -multifasta option, this option can be used to define the path to the directory you want the multifastas written."
+	)
 
 
 
-	args = parser.parse_args(sys.argv[1:])
 
-	if not any([args.out_fasta, args.out_matrix, all([args.out_dir, args.multifasta])]):
-		print("You did not provide enough output instructions. Use -m to output a snp matrix, -f to output an aligned multifasta of concatenated sequences, or both -o and -multifasta to output aligned multifastas of each individual core segment.")
-		sys.exit()
-	if any([args.out_dir, args.multifasta]) and not all([args.out_dir, args.multifasta]):
-		print("you must provide both -o and -multifasta to output aligned multifastas of each individual core segment.")
-		sys.exit()
+args = parser.parse_args(sys.argv[1:])
 
-	if args.multifasta:
-		outdir = args.out_dir + '/' if args.out_dir[-1] != '/' else args.out_dir
-		if not os.path.isdir(outdir):
-			print("Provided output directory does not exist. It will be created.")
-			os.makedirs(outdir)
+if not any([args.out_fasta, args.out_matrix, all([args.out_dir, args.multifasta])]):
+	print("You did not provide enough output instructions. Use -m to output a snp matrix, -f to output an aligned multifasta of concatenated sequences, or both -o and -multifasta to output aligned multifastas of each individual core segment.")
+	sys.exit()
+if any([args.out_dir, args.multifasta]) and not all([args.out_dir, args.multifasta]):
+	print("you must provide both -o and -multifasta to output aligned multifastas of each individual core segment.")
+	sys.exit()
+
+if args.multifasta:
+	outdir = args.out_dir + '/' if args.out_dir[-1] != '/' else args.out_dir
+	if not os.path.isdir(outdir):
+		print("Provided output directory does not exist. It will be created.")
+		os.makedirs(outdir)
 
 
-	with open(args.reference, 'r') as inref:
-		ref_fasta = fasta_to_dict(inref.read())
+with open(args.reference, 'r') as inref:
+	ref_fasta = fasta_to_dict(inref.read())
 
-	query_snps_list = []
-	snp_mat = False
+query_snps_list = []
+snp_mat = False
 
-	ref_snps_obj = snps_object("Reference")
-	for file in args.snps_files:
-		print("Reading in .snps files...")
-		print("Processing .snps file %i of %i" %(args.snps_files.index(file)+1, len(args.snps_files)))
-		genomeid = re.match(args.filename_regex ,file.split(os.path.sep)[-1])[1]
-		ref_snps_obj, query_snps_obj = parse_snps(ref_fasta, ref_snps_obj, file, genomeid)
-		query_snps_list.append(query_snps_obj)
+ref_snps_obj = snps_object("Reference")
+for file in args.snps_files:
+	print("Reading in .snps files...")
+	print("Processing .snps file %i of %i" %(args.snps_files.index(file)+1, len(args.snps_files)))
+	genomeid = re.match(args.filename_regex ,file.split(os.path.sep)[-1])[1]
+	ref_snps_obj, query_snps_obj = parse_snps(ref_fasta, ref_snps_obj, file, genomeid)
+	query_snps_list.append(query_snps_obj)
+
+for i in range(len(query_snps_list)):
+	print("Adding reference sequence at variant sites not found in .snps file to each entry...")
+	print("Processing .snps file %i of %i" %(i+1, len(query_snps_list)))
+	query_snps_list[i].snps = fill_query_dict(ref_snps_obj.snps, query_snps_list[i].snps)
+
+snp_count = 0
+for v in ref_snps_obj.snps.values():
+	for subv in v.values():
+		if isinstance(subv, dict):
+			for subsubv in subv.values():
+				snp_count += 1
+		else:
+			snp_count += 1
+
+if args.out_matrix:
+	print("Building SNP matrix...")
+
+	snp_mat = make_snp_matrix(query_snps_list)
+
+	print("Writing SNP matrix file...")
+
+	# with gzip.open("snp_mat_dict.pkl.gzip", 'wb') as pklout:
+	# 	pickle.dump(snp_mat, pklout, protocol=pickle.HIGHEST_PROTOCOL)
+
+	with open(args.out_matrix, 'w+') as outmat:
+		headers = list(snp_mat["Genome_ID"])
+		for k in sorted(snp_mat.keys()):
+			if k == "Genome_ID":
+				pass
+			else:
+				write_string.append(k + args.matrix_delim + args.matrix_delim.join(list(snp_mat[k][i] for i in snp_mat["Genome_ID"])))
+		outmat.write('\n'.join(write_string))
+
+if not args.whole:
+	if args.out_fasta:
+		if not snp_mat:
+			print("Building SNP matrix...")
+			snp_mat = make_snp_matrix(query_snps_list)
+
+else:
+
+	print("Adding reference sequence at invariant sites to each entry...")
+	ref_snps_obj = fill_ref_dict_with_seq(ref_snps_obj, ref_fasta)
 
 	for i in range(len(query_snps_list)):
-		print("Adding reference sequence at variant sites not found in .snps file to each entry...")
 		print("Processing .snps file %i of %i" %(i+1, len(query_snps_list)))
 		query_snps_list[i].snps = fill_query_dict(ref_snps_obj.snps, query_snps_list[i].snps)
+		query_snps_list[i].snps = {key:{k:v for k,v in sorted(query_snps_list[i].snps[key].items())} for key in sorted(query_snps_list[i].snps)}
+	
+	del ref_snps_obj
 
-	snp_count = 0
-	for v in ref_snps_obj.snps.values():
-		for subv in v.values():
-			if isinstance(subv, dict):
-				for subsubv in subv.values():
-					snp_count += 1
-			else:
-				snp_count += 1
+	print("Building SNP and backbone sequence matrix for whole core genome...")
+	snp_mat = make_snp_matrix(query_snps_list)
 
-	if args.out_matrix:
-		print("Building SNP matrix...")
+	if not args.multifasta:
+		del query_snps_list
 
-		snp_mat = make_snp_matrix(query_snps_list)
+if args.out_fasta:
+	print("Writing fasta file...")
 
-		print("Writing SNP matrix file...")
+	with open(args.out_fasta, 'w+') as outf:
+		fasta_list = []
+		ref_genome = re.match(args.filename_regex ,args.snps_files[0].split(os.path.sep)[-1])[1] # The first genome in the dict will be the reference for the purpose of snp order
+		for k,v in snp_mat.items():
+			if k != "Genome_ID":
+				fasta_list.append(">" + k + '\n' + "".join([v[i] for i in snp_mat[ref_genome].keys()]))
+		outf.write('\n'.join(fasta_list) + '\n')
 
-		# with gzip.open("snp_mat_dict.pkl.gzip", 'wb') as pklout:
-		# 	pickle.dump(snp_mat, pklout, protocol=pickle.HIGHEST_PROTOCOL)
-
-		with open(args.out_matrix, 'w+') as outmat:
-			headers = list(snp_mat["Genome_ID"])
-			for k in sorted(snp_mat.keys()):
-				if k == "Genome_ID":
-					pass
-				else:
-					write_string.append(k + args.matrix_delim + args.matrix_delim.join(list(snp_mat[k][i] for i in snp_mat["Genome_ID"])))
-			outmat.write('\n'.join(write_string))
-		outmat.close()
-
-	if not args.whole:
-		if args.out_fasta:
-			if not snp_mat:
-				print("Building SNP matrix...")
-				snp_mat = make_snp_matrix(query_snps_list)
-
-	else:
-
-		print("Adding reference sequence at invariant sites to each entry...")
-		ref_snps_obj = fill_ref_dict_with_seq(ref_snps_obj, ref_fasta)
-
-		for i in range(len(query_snps_list)):
-			print("Processing .snps file %i of %i" %(i+1, len(query_snps_list)))
-			query_snps_list[i].snps = fill_query_dict(ref_snps_obj.snps, query_snps_list[i].snps)
-			query_snps_list[i].snps = {key:{k:v for k,v in sorted(query_snps_list[i].snps[key].items())} for key in sorted(query_snps_list[i].snps)}
-		
-		del ref_snps_obj
-
-		print("Building SNP and backbone sequence matrix for whole core genome...")
-		snp_mat = make_snp_matrix(query_snps_list)
-
-		if not args.multifasta:
-			del query_snps_list
-
-	if args.out_fasta:
-		print("Writing fasta file...")
-
-		with open(args.out_fasta, 'w+') as outf:
-			fasta_list = []
-			ref_genome = re.match(args.filename_regex ,args.snps_files[0].split(os.path.sep)[-1])[1] # The first genome in the dict will be the reference for the purpose of snp order
-			for k,v in snp_mat.items():
-				if k != "Genome_ID":
-					fasta_list.append(">" + k + '\n' + "".join([v[i] for i in snp_mat[ref_genome].keys()]))
-			outf.write('\n'.join(fasta_list) + '\n')
-		outf.close()
-
-	if args.multifasta:
-		multifasta_snpmat = make_snp_matrix_multifasta(query_snps_list)
-		multifastas_dict = defaultdict(dict) # {contig : {genome_id : sequence}}
-		for entry, subdict in multifasta_snpmat.items():
-			for contig, sequence in subdict.items():
-				multifastas_dict[contig][entry] = sequence
-		for contig, contig_dict in multifastas_dict.items():
-			with open(outdir + contig + '.fna', 'w') as fout:
-				fout.write("".join([">{}\n{}\n".format(k,v) for k,v in contig_dict.items()]))
+if args.multifasta:
+	multifasta_snpmat = make_snp_matrix_multifasta(query_snps_list)
+	multifastas_dict = defaultdict(dict) # {contig : {genome_id : sequence}}
+	for entry, subdict in multifasta_snpmat.items():
+		for contig, sequence in subdict.items():
+			multifastas_dict[contig][entry] = sequence
+	for contig, contig_dict in multifastas_dict.items():
+		with open(outdir + contig + '.fna', 'w') as fout:
+			fout.write("".join([">{}\n{}\n".format(k,v) for k,v in contig_dict.items()]))
 
 
-	stop = time.time()
+stop = time.time()
 
-	print("All done! Found a total of %i variant sites among %i genomes in %.3f seconds." %(snp_count, len(args.snps_files), stop-start))
+print("All done! Found a total of %i variant sites among %i genomes in %.3f seconds." %(snp_count, len(args.snps_files), stop-start))
 
-if __name__ == "__main__":
-	main()
+#if __name__ == "__main__":
+#	main()
